d5c6ddc5a79c246aabe3295fd69db322532697b4
MustafaMalvi <mustafamalvi2@gmail.com>
2026-07-12 12:25:00 +0530

    Set trust proxy for Express app

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const errorHandler = require('./middleware/errorHandler');
 require('dotenv').config();
 
 const app = express();
-
+app.set('trust proxy', 1);
 // ──────────── Security Middleware ────────────
 
 // Allow React frontend to connect
